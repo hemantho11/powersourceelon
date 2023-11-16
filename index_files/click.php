cec94f00b25cea6ce1707804de476010535b5143
hemantho11 <145105045+hemantho11@users.noreply.github.com>
2023-11-16 13:38:19 +0530

    Add files via upload

---
+++ index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('h91nk2g6') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "h91nk2g6"; 
